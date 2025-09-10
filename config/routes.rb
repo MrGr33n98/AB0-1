1087ee35d7ce2c5d65691833d5f39fa3b46108a4
MrGr33n98 <felipehhenriquee@gmail.com>
2025-09-10 20:34:44 -0300

    first commit - setup inicial do projeto Rails + ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   get 'users/profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -16,7 +17,6 @@ Rails.application.routes.draw do
 
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'pages#home'
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,4 @@ if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password',
                     password_confirmation: 'password')
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
