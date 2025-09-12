85c00eecab5ea70fac88860f651e0630d1534d99
MrGr33n98 <felipehhenriquee@gmail.com>
2025-09-11 23:39:11 -0300

    Include products in categories API response

---
+++ app/controllers/api/categories_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  class CategoriesController < ApplicationController
+    def index
+      @categories = Category.all
+      render json: @categories.as_json(include: {
+        companies: { only: [:id, :name, :description, :website, :phone, :address] },
+        products: { only: [:id, :name, :description, :price, :image_url] }
+      })
+    end
+
+    def show
+      @category = Category.find(params[:id])
+      render json: @category.as_json(
+        include: {
+          companies: { only: [:id, :name, :description, :website, :phone, :address] },
+          products: { only: [:id, :name, :description, :price, :image_url] }
+        }
+      )
+    end
+  end
+end
