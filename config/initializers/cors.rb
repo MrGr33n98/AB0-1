43612448770590cc78fb1217aec5180c1aa9f111
MrGr33n98 <felipehhenriquee@gmail.com>
2025-09-14 20:27:46 -0300

    feat: Configure CORS to allow frontend domain access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,10 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # In development, allow requests from any origin
-    origins '*'
-
+    origins 'http://64.225.59.107:3000', 'localhost:3000'
+    
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false,
-      expose: ['Content-Type', 'Content-Disposition', 'Content-Length']
+      credentials: true
   end
 end
