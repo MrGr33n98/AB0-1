c42b93960d49704a7b5dd927edb99f9e096773a0
MrGr33n98 <felipehhenriquee@gmail.com>
2025-09-15 22:00:56 -0300

    Fix products migration to create companies table first

--- db/migrate/20250911061329_create_products.rb
+++ db/migrate/20250911061329_create_products.rb
@@ -1,11 +1,17 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
+    # First create companies table if it doesn't exist
+    create_table :companies do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    # Then create products table
     create_table :products do |t|
       t.string :name
       t.text :description
       t.decimal :price
-      t.references :company, null: false, foreign_key: true
-
+      t.references :company, foreign_key: true
       t.timestamps
     end
   end
