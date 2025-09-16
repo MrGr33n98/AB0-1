38f2e0241792c3d417e45c11b4c3428737e2031c
MrGr33n98 <felipehhenriquee@gmail.com>
2025-09-16 20:25:08 -0300

    Liberando CORS para avaliasolar.com.br

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://64.225.59.107:3000', 'http://localhost:3000', 'http://64.225.59.107:3001', 'http://localhost:3001'
-    
+    origins 'http://64.225.59.107:3000',
+            'http://localhost:3000',
+            'http://64.225.59.107:3001',
+            'http://localhost:3001',
+            'http://www.avaliasolar.com.br',
+            'https://www.avaliasolar.com.br'
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
