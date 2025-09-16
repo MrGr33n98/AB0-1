6d2694c937708d67348456fb2cfdbf1af5aee99d
MrGr33n98 <felipehhenriquee@gmail.com>
2025-09-15 22:23:57 -0300

    fix: CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'] || '*'
+    origins 'http://64.225.59.107:3000', 'http://localhost:3000'
+    
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
